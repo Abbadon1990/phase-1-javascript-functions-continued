75893ada4a3a556842d15e6024d5b910635b1423
Logan Wood <lgnwood@gmail.com>
2023-10-11 17:21:18 -0600

    complete tests

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+function saturdayFun(activity = 'roller-skate', msg = 'This Saturday, I want to') {
+    return `${msg} ${activity}!`
+}
+
+function mondayWork(work = 'go to the office', msg = 'This Monday, I will') {
+    return `${msg} ${work}.`
+}
+
+function wrapAdjective(para = '*') {
+    return function (str = 'special') {
+        return `You are ${para}${str}${para}!`
+    }
+}
